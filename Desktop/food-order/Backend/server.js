dfdf76d33e4e522d61e93b4c067525249064cc0f
manikandan <manikandan110305@gmail.com>
2025-09-07 23:15:20 +0530

    Fix package.json merge conflict

--- Desktop/food-order/Backend/server.js
+++ Desktop/food-order/Backend/server.js
@@ -55,6 +55,11 @@ app.use("/api/sms", sms);
 app.use("/api/restaurants", restaurantRoutes);
 
 
+// ✅ Root route
+app.get("/", (req, res) => {
+  res.send("✅ Backend is running on Render!");
+});
+
 // MongoDB Connect
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
